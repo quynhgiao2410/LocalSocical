660866ae796f7d2c71dcd51e7433343a9dee8063
Nguyên Nguyên <no1.ily1606@gmail.com>
2020-11-05 20:33:22 +0700

    Tuong Nguyen

--- _connect.php
+++ _connect.php
@@ -1,4 +1,11 @@
 <?php
-
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databasename = "localsocical";
+$conn = new mysqli($servername,$username,$password,$databasename);
+if($conn->connect_error){
+    die("Có lỗi khi kết nối với máy chủ CSDL!");
+}
 
 ?>
